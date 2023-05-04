3362ee9d21f10535853688874447f338c6f87bac
João Gabriel Ribeiro Mira <joao.mira@fatec.sp.gov.br>
2023-05-04 19:20:45 -0300

    teste associar

--- atvi-pl-typescript/src/negocio/cadastroPet.ts
+++ atvi-pl-typescript/src/negocio/cadastroPet.ts
@@ -1,7 +1,5 @@
 import Entrada from "../io/entrada"
-import cliente from "../modelo/cliente"
 import Cliente from "../modelo/cliente"
-import CPF from "../modelo/cpf"
 import Empresa from "../modelo/empresa"
 import Pet from "../modelo/pet"
 import Cadastro from "./cadastro"
@@ -17,20 +15,30 @@ export default class CadastroPet extends Cadastro {
         this.entrada = new Entrada()
         this.empresa = new Empresa()
     }
-
+    
+    private selecionarCliente(): Cliente {
+        let cpf = this.entrada.receberTexto('Digite o CPF do cliente: ')
+        let selecionadorCliente = new Selecionador(this.empresa.getClientes)
+        let cliente = selecionadorCliente.selecionar(cpf)
+        return cliente;
+    }
 
     public cadastrar(): void {
         console.log(`\nInício do cadastro do pet`);
-        let cpfTutor = this.entrada.receberTexto('Digite o CPF do tutor: ')
-        let selecionadorTutor = new Selecionador(this.empresa.getClientes)
-        let tutor = selecionadorTutor.selecionar(cpfTutor)
         let nome = this.entrada.receberTexto(`Por favor informe o nome do pet: `)
         let tipo = this.entrada.receberTexto(`Por favor informe o tipo do pet: `)
         let raca = this.entrada.receberTexto(`Por favor informe a raça do pet: `);
         let genero = this.entrada.receberTexto(`Por favor informe o gênero do pet: `);
-        let pet = new Pet(nome, tipo, raca, genero);
-        this.pets.push(pet)
         
-        console.log(`\nCadastro concluído :)\n`);
+        let cliente = this.selecionarCliente();
+
+        if (cliente) {
+            let pet = new Pet(nome, tipo, raca, genero);
+            cliente.adicionarPet(pet);
+            console.log(`\nPet cadastrado e associado ao cliente ${cliente.nome}.\n`);
+        } else {
+            console.log(`\nCliente não encontrado. O pet não foi cadastrado.\n`);
+        }
     }
 }
+
