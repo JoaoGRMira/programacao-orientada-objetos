0fca0a269a7e07b86346ef76139a67d22e0af12f
Rebeca Gama <rebeca.moraes@fatec.sp.gov.br>
2023-06-17 21:52:04 -0300

    atualização texto

--- atvi-pl-typescript/src/negocio/edicoes/editorCliente.ts
+++ atvi-pl-typescript/src/negocio/edicoes/editorCliente.ts
@@ -8,9 +8,9 @@ export default class EditorCliente {
     }
     public editar(cliente: Cliente): void {
         console.log(`\nInício da edição do cliente`);
-        let nome = this.entrada.receberTexto(`Por favor informe o nome do cliente: `)
+        let nome = this.entrada.receberTexto(`Por favor informe o novo nome do cliente: `)
         cliente.nome = nome
-        let nomeSocial = this.entrada.receberTexto(`Por favor informe o nome social do cliente: `)
+        let nomeSocial = this.entrada.receberTexto(`Por favor informe o novo nome social do cliente: `)
         cliente.nomeSocial = nomeSocial
         console.log(`\Edição concluída :)\n`);
     }
